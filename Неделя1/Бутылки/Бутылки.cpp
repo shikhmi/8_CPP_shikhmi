03a486b4e7bbaf0978589c11aed4444e0e0ef07e
shikhmi <ashikhmina.na@phystech.edu>
2023-09-16 00:56:05 +0300

    edited

---
+++ Неделя1/Бутылки/Бутылки.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+int main()
+{
+    int floors = 100;
+    int count = 0;
+    int i = 1;
+    int sum = 0;
+    while (sum <= floors) {
+        sum = sum + i;
+        i = i + 1;
+        count = count + 1;
+    }
+    std::cout << count;
+}
+
