59b36e4dcad021aa6b0c8b500b7de03982eb7b4f
shikhmi <ashikhmina.na@phystech.edu>
2023-10-16 02:42:24 +0300

    ocd sorted

--- Неделя 5/Class_OCD/Class_OCD.cpp
+++ Неделя 5/Class_OCD/Class_OCD.cpp
@@ -13,6 +13,7 @@
 //#include "Drugs.cpp"
 #include <iostream>
 #include <string>
+#include "Diseases.cpp"
 
 /*class Diseases
 {
@@ -31,85 +32,7 @@ public:
     int obsessive_thoughts();
 };*/
 
-namespace Diseases
-{
 
-    class Disease {
-    public:
-
-        virtual int apathy()
-        {
-            return -1;
-        };
-
-        virtual std::string storytelling()
-        {
-            return "i'm sick :(";
-        };
-    };
-
-    class OCD : public Disease {
-    private:
-        int excessive_pedantry()
-        {
-            return 7;
-        };
-    public:
-        virtual int frightening_drives()
-        {
-            return 5;
-        }
-
-        int apathy() override
-        {
-            return 3;
-        };
-
-        std::string storytelling() override
-        {
-            return "Did i close the door?";
-        };
-
-    };
-
-    class PanicAttacks : public OCD {
-        int frightening_drives() override
-        {
-            return 10;
-        };
-
-        int panic_attacks()
-        {
-            return 10;
-        }
-        int apathy() override
-        {
-            return 6;
-        };
-    };
-
-    class Anorexia : public Disease {
-        int fasting()
-        {
-            return 10;
-        }
-        int apathy() override
-        {
-            return 8;
-        };
-    };
-
-    class Bulimia : public Disease {
-        int overeating()
-        {
-            return 10;
-        }
-        int apathy() override
-        {
-            return 9;
-        };
-    };
-};
 int main() 
 {
     Diseases::OCD* myOCD = new Diseases::OCD();
